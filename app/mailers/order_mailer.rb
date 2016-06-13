593e1b76a3b40bfb86de36a62cc72d64ec560b00
XueMingxiang <xuemx@cleverloop.com>
2016-06-13 14:31:42 +0800

    remove font files

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,6 @@
 class OrderMailer < ApplicationMailer
 
   def send_email_to_admin
-    mail to: "327110424@163.com", subject: "新订单到达"
+    mail to: "18810607124@163.com", subject: "新订单到达"
   end
 end
